b12fd07280dc481a32dfffd9e45670ad1499a34f
Tukue Gebregergis <tukue.geb@gmail.com>
2025-12-19 14:10:41 +0100

    Resolve Next app merge conflicts

--- next/app/layout.tsx
+++ next/app/layout.tsx
@@ -1,7 +1,10 @@
 import "./globals.css";
 import { Providers } from "./providers";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import { ReactNode } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Meta Tag Analyzer",
@@ -11,7 +14,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
     </html>
--- next/tailwind.config.ts
+++ next/tailwind.config.ts
@@ -3,5 +3,10 @@ import type { Config } from "tailwindcss";
 
 export default {
   ...baseConfig,
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "../client/src/**/*.{js,jsx,ts,tsx}"]
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "../client/src/**/*.{js,jsx,ts,tsx}",
+  ],
 } satisfies Config;
