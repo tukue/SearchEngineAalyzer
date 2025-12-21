936f36be4471b52f428e51db6a0f58c409dd71e4
Tukue Gebregergis <tukue.geb@gmail.com>
2025-12-21 22:40:49 +0100

    Adjust Next alias resolution for shared components

--- next/next.config.js
+++ next/next.config.js
@@ -2,6 +2,9 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    externalDir: true,
+  },
   webpack(config) {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
