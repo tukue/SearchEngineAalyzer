af790f0216a633da92603069d7f1caf37c400ffa
Tukue Gebregergis <tukue.geb@gmail.com>
2026-01-22 15:00:42 +0100

    updating  code review for mvp features

--- client/src/components/ui/badge.tsx
+++ client/src/components/ui/badge.tsx
@@ -12,7 +12,7 @@ const badgeVariants = cva(
           "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
         secondary:
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        critical:
+        caution:
           "border-transparent bg-caution text-caution-foreground hover:bg-caution/80",
         outline: "text-foreground",
         success: "border-transparent bg-green-100 text-green-900 hover:bg-green-200",
